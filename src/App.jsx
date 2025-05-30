45fab7c7baa7f41ea05ae5fabeb07ca204bb2877
Ignacio Martinez <ign.martinezc@gmail.com>
2025-05-29 20:12:39 -0400

    home color

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import Economia from './pages/Economia'
 
 export default function App() {
   return (
-    <>
+    <div className="min-h-screen bg-gray-100 text-gray-800">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/notas" element={<Notas />} />
-        <Route path="/economia" element={<Economia />} />
-      </Routes>
-    </>
+      <main className="max-w-4xl mx-auto px-4 py-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/notas" element={<Notas />} />
+          <Route path="/economia" element={<Economia />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
